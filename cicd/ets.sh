e2f9ead07f14b73685a50d5e055f9fcb60f9953e
northdecoder <northdecoder@users.noreply.github.com>
2021-04-13 22:59:48 +0000

    fix(cicd): remove leading quote
    - add setup sequence

--- cicd/ets.sh
+++ cicd/ets.sh
@@ -8,6 +8,32 @@ echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
 echo "~ Example test script                                     ~"
 echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
 
+
+echo -e "\n\n\n"
+echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
+echo "~ setup commands                                          ~"
+echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
+# find node version from package.json
+# remove node
+# remove >=
+# remove trailing ,
+# remove excess whitespace
+# remove leading quote "
+NODE_VERSION=$(  cat package.json | grep '"node":' | sed 's/"node"://' | sed 's/">=//' | sed 's/",//' | sed -r 's/\s+//g' | sed 's/"//' )
+echo "From package.json the node version is "$NODE_VERSION
+#
+echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ "
+nvm install $NODE_VERSION
+#
+echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ "
+npm --depth 10 update
+echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ "
+npx recursive-install
+
+
+
+
+
 echo -e "\n\n\n"
 echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
 echo "~ ES Lint                                                 ~"
